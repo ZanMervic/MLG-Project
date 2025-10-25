d1bffe826db08c8f41341d26cec171624a4ac457
Tadej Jeršin <104827264+tadejjersin@users.noreply.github.com>
2025-10-25 12:02:00 +0200

    added hold detection from screenshots

---
+++ screenshot_processer.py
@@ -0,0 +1,97 @@
+from PIL import Image
+from collections import Counter
+import numpy as np
+
+path = "Screenshots/3COIN_screenshot.png"
+
+def top_colors_in_box(img, x, y, radius, top_n=6):
+    width, height = img.size
+    x_min = max(x - radius, 0)
+    x_max = min(x + radius, width - 1)
+    y_min = max(y - radius, 0)
+    y_max = min(y + radius, height - 1)
+    region = img.crop((x_min, y_min, x_max + 1, y_max + 1))
+    pixels = np.array(region).reshape(-1, 3)
+    counts = Counter(map(tuple, pixels))
+    total = sum(counts.values())
+    normalized = [(color, count / total) for color, count in counts.items()]
+    normalized.sort(key=lambda x: x[1], reverse=True)
+    return normalized[:top_n]
+
+def color_distance(c1, c2):
+    return np.sum(np.abs(np.array(c1) - np.array(c2)))
+
+def process_image(path, max_color_dist=100):
+    img = Image.open(path)
+    img = img.convert("RGB")
+
+    width, height = img.size
+
+    # get corners of the yellow board
+    yellow = (238, 223, 80)
+    # Loop through all pixels forward
+    flag = False
+    for x in range(width):
+        for y in range(height):
+            rgb = img.getpixel((x, y))
+            if color_distance(rgb, yellow) < max_color_dist:
+                upper_left = (x, y)
+                flag = True 
+                break
+        if flag:
+            break
+    # Loop through all pixels backward
+    flag = False
+    for x in range(width)[::-1]:
+        for y in range(height)[::-1]:
+            rgb = img.getpixel((x, y))
+            if color_distance(rgb, yellow) < max_color_dist:
+                lower_right = (x, y)
+                flag = True 
+                break
+        if flag:
+            break
+    
+    # get middle of A18 and horizontal and vertical steps to next 
+    left_len = (142 / 1015) * (lower_right[0] - upper_left[0])
+    step_horizontal = (79 / 1015) * (lower_right[0] - upper_left[0])
+
+    upper_len = (138 / 1568) * (lower_right[1] - upper_left[1])
+    step_vertical = (78 / 1568) * (lower_right[1] - upper_left[1])
+
+    start_x = upper_left[0] + left_len 
+    start_y = upper_left[1] + upper_len 
+
+    # get approx radius of the red/green/blue circles
+    radius = int((61 / 1568) * (lower_right[1] - upper_left[1]))
+
+    # process A-K 1-18
+    holds = {"start":[], "end":[], "middle":[]}
+    red = (244, 67, 54)
+    blue = (41, 98, 255)
+    green = (76, 175, 80)
+    for i, a in enumerate("ABCDEFGHIJK"):
+        for j, n in enumerate(range(1, 19)[::-1]):
+            x = int(start_x + i * step_horizontal)
+            y = int(start_y + j * step_vertical)
+            top_n_colors = top_colors_in_box(img, x, y, radius)
+            red_p = sum([p for color, p in top_n_colors if color_distance(color, red) < max_color_dist])
+            blue_p = sum([p for color, p in top_n_colors if color_distance(color, blue) < max_color_dist])
+            green_p = sum([p for color, p in top_n_colors if color_distance(color, green) < max_color_dist])
+            if red_p > 0.15:
+                holds["end"].append(f"{a}{n}")
+            if blue_p > 0.15:
+                holds["middle"].append(f"{a}{n}")
+            if green_p > 0.15:
+                holds["start"].append(f"{a}{n}")
+            # for color, p in top_n_colors:
+            #     if p > 0.15:
+            #         if color_distance(color, red) < max_color_dist:
+            #             holds["end"].append(f"{a}{n}")
+            #         if color_distance(color, blue) < max_color_dist:
+            #             holds["middle"].append(f"{a}{n}")
+            #         if color_distance(color, green) < max_color_dist:
+            #             holds["start"].append(f"{a}{n}")
+            #         if a == "E" and n == 18:
+            #             print(color, color_distance(color, red))
+    return holds
